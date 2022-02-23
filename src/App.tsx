b6b80b25dc5317c098fedfa4fc9395ad76405655
Maciek <msitkowski94@gmail.com>
2022-02-23 20:00:29 +0100

    basic, styled example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,79 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from 'react';
+import { WithContext as ReactTags } from 'react-tag-input';
+
+import COUNTRIES from './countries.json';
+
+const suggestions = COUNTRIES.map(country => {
+  return {
+    id: country,
+    text: country,
+  };
+});
+
+const KeyCodes = {
+  comma: 188,
+  enter: 13,
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
+const App = () => {
+  const [tags, setTags] = React.useState([
+    { id: 'Thailand', text: 'Thailand' },
+    { id: 'India', text: 'India' },
+    { id: 'Vietnam', text: 'Vietnam' },
+    { id: 'Turkey', text: 'Turkey' },
+  ]);
+
+  const handleDelete = i => {
+    setTags(tags.filter((tag, index) => index !== i));
+  };
+
+  const handleAddition = tag => {
+    setTags([...tags, tag]);
+  };
+
+  const handleDrag = (tag, currPos, newPos) => {
+    const newTags = tags.slice();
+
+    newTags.splice(currPos, 1);
+    newTags.splice(newPos, 0, tag);
+
+    // re-render
+    setTags(newTags);
+  };
+
+  const handleTagClick = index => {
+    console.log('The tag at index ' + index + ' was clicked');
+  };
+
+  return (
+    <div className="mx-auto mt-12 max-w-sm">
+      <div>
+        <ReactTags
+          tags={tags}
+          suggestions={suggestions}
+          delimiters={delimiters}
+          handleDelete={handleDelete}
+          handleAddition={handleAddition}
+          handleDrag={handleDrag}
+          handleTagClick={handleTagClick}
+          inputFieldPosition="bottom"
+          autocomplete
+          classNames={{
+            tags: 'w-full',
+            selected: 'flex flex-wrap gap-2 mb-2',
+            tag: 'inline-flex items-center gap-2 p-2 bg-gray-200 rounded-lg',
+            tagInputField:
+              'border-gray-500 border-2 rounded-lg px-4 py-2 w-full',
+            // ⚠️ Suggestions are styled in `index.css`, so don't use `suggestions` prop,
+            // because they'll be overwritten.
+            // suggestions <- Don't use it!
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
